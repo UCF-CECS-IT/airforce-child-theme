22530dfc34f6ba25037ead759c7bfd92a8210176
Paul <paulgsepulveda@gmail.com>
2022-03-10 13:32:16 -0500

    0.1.2 added row limit for Contact form

--- functions.php
+++ functions.php
@@ -11,3 +11,11 @@ include_once AIRFORCEWP_THEME_DIR . 'includes/meta.php';
 // Add other includes to this file as needed.
 include_once AIRFORCEWP_THEME_DIR . 'includes/airforce-nav.php';
 include_once AIRFORCEWP_THEME_DIR . 'includes/options.php';
+
+// Change textarea rows to 3 instead of 10 in Gravity Forms
+add_filter( 'gform_field_content', function ( $field_content, $field ) {
+    if ( $field->type == 'textarea' ) {
+        return str_replace( "rows='10'", "rows='3'", $field_content );
+    }
+    return $field_content;
+}, 10, 2 );
