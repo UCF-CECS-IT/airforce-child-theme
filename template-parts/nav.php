73bd01f39abfbe33453103e1617655c31677cfe6
Paul <paulgsepulveda@gmail.com>
2022-03-24 10:46:59 -0400

    updated page header bar

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -8,7 +8,9 @@ if ( !$image ) {
 	$container_class = 'bg-inverse';
 }
 
-$header_img = get_field( 'theme_header', 'option' ) ?? AIRFORCEWP_THEME_IMG_URL . '/rotc-logo.png';
+$header_img_left = get_field( 'theme_header_left', 'option' ) ?? AIRFORCEWP_THEME_IMG_URL . '/rotc-logo.png';
+$header_img_right = get_field( 'theme_header_right', 'option' ) ?? AIRFORCEWP_THEME_IMG_URL . '/airforce-logo.png';
+
 $header_text = get_bloginfo( 'name' );
 $header_subtext = get_bloginfo( 'description' );
 $header_link = get_bloginfo( 'url' );
@@ -28,18 +30,22 @@ $menu = wp_nav_menu( array(
 
 <!-- set bg  -->
 <div class="<?php echo $container_class; ?> header-wrapper">
-
-	<!-- header image  -->
-	<?php if ( $header_img ): ?>
-		<div class="py-3 text-center">
-			<a href="<?php echo $header_link; ?>"><img class="header-img" src="<?php echo $header_img; ?>"></a>
-		</div>
-	<?php endif; ?>
-
 	<!-- header -text -->
-	<div>
-		<h1 class="h3 text-center text-white"><span class="font-serif"><?php echo $header_text; ?></span></h1>
-		<h6 class="text-white text-center font-weight-normal"><?php echo $header_subtext; ?></h6>
+	<div class="d-flex justify-content-around">
+		<!-- header image  -->
+		<div class="">
+			<a href="<?php echo $header_link; ?>"><img class="header-img" src="<?php echo $header_img_left; ?>"></a>
+		</div>
+
+		<div class="header-text">
+			<h1 class="display-4 font-weight-light text-center text-white"><?php echo $header_text; ?></h1>
+			<h6 class="text-white text-center font-weight-normal"><?php echo $header_subtext; ?></h6>
+		</div>
+
+		<!-- header image  -->
+		<div class="">
+			<a href="<?php echo $header_link; ?>"><img class="header-img" src="<?php echo $header_img_right; ?>"></a>
+		</div>
 	</div>
 
 	<div class="container">
